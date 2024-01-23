75e9ae4ee17c7e21792cfb8f9038dac3f4f741c7
colin.pigott.github <colin.pigott.github@gmail.com>
2024-01-23 20:36:42 +0000

    Updated example with new test data.

--- examples/example-018.cpp
+++ examples/example-018.cpp
@@ -4,7 +4,7 @@
 //
 // g++ -Wall -o example-018.out example-018.cpp star.cpp math_misc.cpp time_details.cpp api_interface.cpp coord_conversion.cpp comet_details.cpp sun.cpp moon.cpp planets.cpp
 
-#include "api_interface.hpp"
+#include "api_interface.h"
 #include <iostream>
 #include <iomanip>
 
@@ -17,14 +17,40 @@ int main(void)
   api_interface api;
   double *output;
 
-  // The position is for Jupiter on 22nd Nov 1988 at 00:00:00
+  // Data for 2024/01/01 00:00:00 UT
+  //	object		longitude     latitiude  	Distance(AU)
+  // ===========        =========     =========  	============
+  // Sun (Geo)		280.038992	 0.000162	0.9833183
+  // Earth (Helio)	100.04627	-0.00016	0.9833183
+  //
+  // Jupiter (Helio)	 45.83832	-1.06566	4.9848972
+  // Jupiter (Geo)	 35.582395	-1.185480	4.4814715
+  //
+  // The source:
+  // https://eco.mtk.nao.ac.jp/koyomi/cande/index.html.en
+  
+  
+  // Test Geo co-ords to Helio
   // Convert the geo ecl co-ords for the object and the origin of the new co-ord system(Sun) and convert to the helio co-ords.
-  //                                           geo co-ord Jupiter                    geo co-ord Sun
-  //                                :--------------------------------------:  :--------------------------------------:
-  output = api.helio_ecl_to_geo_ecl(61.2007462, -1.0319223, 4.03398255097488, 239.9074184, 0.0001560, 0.98758155072300);
+  //                                         geo co-ord Jupiter            geo co-ord Sun 
+  //                                :-----------------------------:  :-----------------------------:
+  output = api.helio_ecl_to_geo_ecl(35.582395, -1.185480, 4.4814715, 280.038992, 0.000162, 0.9833183);	
 
   // These lines are used for diagnostic reasons.
-  cout << "example-018" << endl;
+  cout << "Output (Geo to Helio):" << endl;
+  cout << "Calculated helio lambda   = " << setprecision(DISPLAY_PRECISION) << output[0] << endl;
+  cout << "Calculated helio beta     = " << setprecision(DISPLAY_PRECISION) << output[1] << endl;
+  cout << "Calculated helio distance = " << setprecision(DISPLAY_PRECISION) << output[2] << endl;
+  cout << endl;
+
+  // Test Helio co-ords to Geo
+  // Convert the helio ecl co-ords for the object and the origin of the new co-ord system(Sun) and convert to the helio co-ords.
+  //                                    helio co-ord Jupiter            Helio co-ord Earth 
+  //                                :---------------------------:  :----------------------------:
+  output = api.helio_ecl_to_geo_ecl(45.83832, -1.06566, 4.9848972, 100.04627, -0.00016, 0.9833183);
+
+  // These lines are used for diagnostic reasons.
+  cout << "Output (Helio to Geo):" << endl;
   cout << "Calculated helio lambda   = " << setprecision(DISPLAY_PRECISION) << output[0] << endl;
   cout << "Calculated helio beta     = " << setprecision(DISPLAY_PRECISION) << output[1] << endl;
   cout << "Calculated helio distance = " << setprecision(DISPLAY_PRECISION) << output[2] << endl;
